7a39638767aee0d1fd53a98535b572c410ea8d02
thomashvc <145122440+thomashvc@users.noreply.github.com>
2024-02-02 11:53:30 +0100

    Update H1O01.js

--- JS/H1/H1O01.js
+++ JS/H1/H1O01.js
@@ -1,26 +1,77 @@
+var score = 0;
+var balletjes = [];
+
 function setup() {
-  canvas = createCanvas(450,450);
-  background('grey');
+  canvas = createCanvas(600, 450);
   canvas.parent('processing');
-  noLoop();
+  textFont("Verdana");
+  textSize(14);
+
+  // genereer 4 balletjes bij het laden van de pagina
+  for (var i = 0; i < 4; i++) {
+    balletjes.push({
+      x: random(50, width - 50),
+      y: random(50, height - 50),
+      kleur: color(random(255), random(255), random(255))
+    });
+  }
+
+  // maak een knop voor het opnieuw beginnen van het spel
+  resetButton = createButton('Opnieuw beginnen');
+  resetButton.position(130, 270);
+  resetButton.mousePressed(resetGame);
 }
 
 function draw() {
-  // groene cirkel zonder rand
-  
-  noStroke();
-  fill('green');
-  ellipse(225,225,400);
-  
-  noStroke();
-  fill('white');
-  ellipse(225,225,300);
-  // witte rechthoek met rode rand
-  
-  stroke('green');
-  fill('white');
-  strokeWeight(10);
-  rect(125,125,200,200);
+  background('lavender');
+  fill('black');
 
-  
+  text("Score: " + score, 400, 20);
+
+  // teken alle balletjes
+  for (var i = 0; i < balletjes.length; i++) {
+    push();
+    translate(balletjes[i].x, balletjes[i].y);
+    fill(balletjes[i].kleur);
+    ellipse(0, 0, 50);
+    pop();
+  }
+
+  // genereer een nieuw balletje als er geen balletjes meer zijn
+  if (balletjes.length < 4) {
+    balletjes.push({
+      x: random(50, width - 50),
+      y: random(50, height - 50),
+      kleur: color(random(255), random(255), random(255))
+    });
+  }
+}
+
+function resetGame() {
+  score = 0;
+  balletjes = [];
+
+  // genereer 4 nieuwe balletjes
+  for (var i = 0; i < 4; i++) {
+    balletjes.push({
+      x: random(50, width - 50),
+      y: random(50, height - 50),
+      kleur: color(random(255), random(255), random(255))
+    });
+  }
+}
+
+function mousePressed() {
+  for (var i = 0; i < balletjes.length; i++) {
+    if (dist(mouseX, mouseY, balletjes[i].x, balletjes[i].y) < 25) {
+      score++;
+      balletjes.splice(i, 1);
+    }
+  }
+}
+
+function balletje(x, y, kleur) {
+  this.x = x;
+  this.y = y;
+  this.kleur = kleur;
 }
